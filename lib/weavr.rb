08374d9c15ff0eefd2387ab9efdf34f016f3d68c
Travis Dempsey <dempsey.travis@gmail.com>
2014-07-03 16:20:18 -0500

    load classes, set defaults

--- lib/weavr.rb
+++ lib/weavr.rb
@@ -2,5 +2,27 @@ require 'faraday'
 require 'faraday_middleware'
 
 require 'weavr/connection'
+require 'weavr/resource'
+require 'weavr/response'
 require 'weavr/version'
 
+module Weavr
+
+  DEFAULT_USERNAME = 'admin'
+  DEFAULT_PASSWORD = 'admin'
+  AMBARI_HOST      = 'localhost'
+  AMBARI_PORT      = 8080
+
+  def self.default_configuration
+    {
+      username: DEFAULT_USERNAME,
+      password: DEFAULT_PASSWORD,
+      host:     AMBARI_HOST,
+      port:     AMBARI_PORT,
+    }
+  end
+
+  def self.connection
+    @connection ||= Connection.new
+  end
+end
