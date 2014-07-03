02e1d61a0fe59d328674355f8b692698f6d69d15
Travis Dempsey <dempsey.travis@gmail.com>
2014-07-03 17:54:06 -0500

    changed connection methods to model queries

--- lib/weavr/connection.rb
+++ lib/weavr/connection.rb
@@ -7,10 +7,10 @@ module Weavr
       options = Weavr.default_configuration.merge(options)
       @host = options[:host]
       @port = options[:port]
-      establish_connection!(options[:username], options[:password])
+      configure_connection(options[:username], options[:password])
     end
 
-    def establish_connection!(user, password)
+    def configure_connection(user, password)
       @connection ||= Faraday.new(url: base_url, headers: request_headers) do |conn|
         conn.request  :json
         conn.request  :basic_auth, user, password
@@ -28,20 +28,16 @@ module Weavr
     end
 
     def resource(action, path)
-      resp = connection.send(action, path)
-      Response.new(resp.status, resp.headers, resp.body)
-    end
-
-    def hosts
-      resource(:get, 'hosts')
+      connection.send(action, path)
+      # Response.new(resp.status, resp.headers, resp.body)
     end
 
     def clusters
-      resource(:get, 'clusters')
+      Collection.of(Cluster).receive resource(:get, 'clusters').body
     end
 
     def cluster name
-      resource(:get, "clusters/#{name}")
+      Cluster.receive resource(:get, "clusters/#{name}").body
     end
   end
 end
