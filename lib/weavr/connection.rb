8710d6c9d2509942729018ec4007709d6b828a8c
Travis Dempsey <dempsey.travis@gmail.com>
2014-07-03 16:19:01 -0500

    break out connection methods

--- lib/weavr/connection.rb
+++ lib/weavr/connection.rb
@@ -1,16 +1,47 @@
 module Weavr
   class Connection
 
-    attr_reader :connection
+    attr_reader :connection, :host, :port
 
-    def initialize
-      @connection = Faraday.new(url: 'http://localhost:8080/api/v1') do |conn|
+    def initialize(options = {})
+      options = Weavr.default_configuration.merge(options)
+      @host = options[:host]
+      @port = options[:port]
+      establish_connection!(options[:username], options[:password])
+    end
+
+    def establish_connection!(user, password)
+      @connection ||= Faraday.new(url: base_url, headers: request_headers) do |conn|
         conn.request  :json
-        conn.request  :basic_auth, 'admin', 'admin'
+        conn.request  :basic_auth, user, password
         conn.response :json
         conn.adapter  Faraday.default_adapter
       end
     end
+
+    def base_url
+      "http://#{host}:#{port}/api/v1"
+    end
+
+    def request_headers
+      { 'X-Requested-By' => "Weavr #{Weavr::VERSION}" }
+    end
+
+    def resource(action, path)
+      resp = connection.send(action, path)
+      Response.new(resp.status, resp.headers, resp.body)
+    end
+
+    def hosts
+      resource(:get, 'hosts')
+    end
+
+    def clusters
+      resource(:get, 'clusters')
+    end
+
+    def cluster name
+      resource(:get, "clusters/#{name}")
+    end
   end
 end
-
