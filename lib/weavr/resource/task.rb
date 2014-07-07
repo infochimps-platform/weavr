cb12cf319142f56eb39637759a98b3b1fabb13e9
Travis Dempsey <dempsey.travis@gmail.com>
2014-07-07 15:24:35 -0500

    complete task model

--- lib/weavr/resource/task.rb
+++ lib/weavr/resource/task.rb
@@ -1,7 +1,19 @@
 module Weavr
   class Task < Resource
-    field :cluster_name, String
-    field :id,           Integer
-    field :request_id,   Integer
+    field :cluster_name,   String
+    field :attempt_cnt,    Integer
+    field :id,             Integer
+    field :request_id,     Integer
+    field :command,        String
+    field :command_detail, String
+    field :exit_code,      Integer
+    field :host_name,      String
+    field :request_id,     String
+    field :role,           String
+    field :stage_id,       String
+    field :start_time,     Integer # Needs to be Date
+    field :status,         String
+    field :stdout,         String
+    field :structured_out, String
   end
 end
