e2d6ad7807c51c81e87bde9f885a78a8a8ece0b2
Travis Dempsey <dempsey.travis@gmail.com>
2014-07-07 11:57:33 -0500

    simple response wrapper and validation

--- lib/weavr/response.rb
+++ lib/weavr/response.rb
@@ -1,12 +1,40 @@
 module Weavr
   class Response
+    include Gorillib::Model
 
-    attr_reader :body, :headers, :status
+    field :status,  Integer
+    field :headers, Hash
+    field :body,    Hash
+    field :errors,  Array, default: []
 
-    def initialize(status, headers, body)
-      @status  = status
-      @headers = headers
-      @body    = body
+    def self.handle(status, headers, body)
+      puts "[debug] #{status}"
+      self.receive(status: status, headers: headers, body: body).validate!
+    end
+
+    # Really wish there was validations for these
+    def receive_status param
+      case param
+      when 200
+        @status = param
+      when 400
+        errors << { status: 400 }
+      when 404
+        errors << { status: 404 }
+      when 500
+        errors << { status: 500 }
+      end
+    end
+
+    # def receive_headers param
+    # end
+
+    # def receive_body param
+    # end
+
+    def validate!
+      raise RequestError.new error_message unless errors.empty?
+      body
     end
 
     def error_message
