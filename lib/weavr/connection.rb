c96a41839fcff85416790176428f3efc1015cb4c
Travis Dempsey <dempsey.travis@gmail.com>
2014-07-11 17:21:58 -0500

    simplified connection; ported methods to Weavr

--- lib/weavr/connection.rb
+++ lib/weavr/connection.rb
@@ -1,20 +1,19 @@
 module Weavr
   class Connection
 
-    attr_reader :connection, :host, :port
+    attr_reader :host, :port, :http
 
     def initialize(options = {})
-      options = Weavr.default_configuration.merge(options)
-      @host   = options[:host]
-      @port   = options[:port]
-      configure_connection(options[:username], options[:password])
+      @host = options[:host]
+      @port = options[:port]
+      establish_connection(options[:username], options[:password])
     end
 
-    def configure_connection(user, password)
-      @connection ||= Faraday.new(url: base_url, headers: request_headers) do |conn|
+    def establish_connection(user, password)
+      @http ||= Faraday.new(url: base_url, headers: request_headers) do |conn|
         conn.request  :basic_auth, user, password
         conn.response :json
-        conn.response :logger, Weavr.logger
+        conn.response :logger, Weavr.log
         conn.adapter  Faraday.default_adapter
       end
     end
@@ -28,27 +27,14 @@ module Weavr
     end
 
     def resource(action, path, data = {})
-      resp = connection.send(action, path) do |req|
-        Weavr.logger.info "payload: #{data}" if data
+      resp = http.send(action, path) do |req|
+        Weavr.log.info "payload: #{data}" if data
         req.body = data.to_json if data
       end
-      Weavr.logger.info "body: #{resp.body}"
+      Weavr.log.info "body: #{resp.body}"
       Response.handle(resp.status, resp.headers, resp.body)
     rescue Faraday::ConnectionFailed
       raise ConnectionError.new "Could not reach Ambari API at #{host}:#{port}"
-    end
-
-    def clusters
-      Collection.of(Cluster).receive resource(:get, 'clusters')
-    end
-
-    def cluster name
-      Cluster.receive resource(:get, "clusters/#{name}")
-    end
-
-    def create_cluster name
-      cluster = Cluster.receive(cluster_name: name, href: File.join(base_url, 'clusters', name))
-      cluster.create
     end
   end
 end
