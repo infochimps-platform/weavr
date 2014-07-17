895871c986093a4c15f258d16c2916de19708033
Travis Dempsey <dempsey.travis@gmail.com>
2014-07-17 14:16:45 -0500

    use new resource class loader

--- lib/weavr/resource/class_map.rb
+++ lib/weavr/resource/class_map.rb
@@ -1,14 +1,10 @@
 # Declare all classes ahead of time to avoid dependency problems
-module Weavr
-  Cluster       = Class.new Resource
-  Collection    = Class.new Resource
-  Component     = Class.new Resource
-  Configuration = Class.new Resource
-  Host          = Class.new Resource
-  HostRole      = Class.new Resource
-  Request       = Class.new Resource
-  Service       = Class.new Resource
-  Task          = Class.new Resource
-end
+%w[ Cluster Collection Component Configuration Host
+    HostRole OperatingSystem Repository Request
+    Service ServiceComponent  Stack StackConfiguration
+    StackService StackVersion Task
+].each{ |resource| Weavr::Resource.predefine_class resource }
+
+Weavr::Resource.load_definitions!
 
 
