eecbb3bca1950e1c554ccab718ac54cbc1e77a5d
Travis Dempsey <dempsey.travis@gmail.com>
2014-07-18 10:22:59 -0500

    have collections inherit from Collection

--- lib/weavr/resource/collection.rb
+++ lib/weavr/resource/collection.rb
@@ -1,7 +1,7 @@
 module Weavr
   class Collection < Resource
     def self.of kind
-      coll = Class.new(Resource) do
+      coll = Class.new(self) do
         field :items, Array, of: kind
       end
       Weavr.const_set("#{kind.to_s.demodulize}Collection", coll)
